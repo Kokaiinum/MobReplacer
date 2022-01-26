c649a16f7633c4a8e1cdb7c0b8b1e331a501ceb7
Kokaiinum <42316813+Kokaiinum@users.noreply.github.com>
2022-01-26 02:10:40 +0000

    bump version

--- src/PluginCode/KK_MobReplacer.cs
+++ src/PluginCode/KK_MobReplacer.cs
@@ -22,7 +22,7 @@ namespace KK_MobReplacer {
 
         public const string PluginName = "Generic Mob Replacer";
         public const string GUID = "mobReplacer";
-        public const string Version = "1.0.0";
+        public const string Version = "1.0.1";
 
         public const string FileExtension = ".png";
         public const string Filter = "Character cards (*.png)|*.png|All files|*.*";
